a92a3009df903a5c5ae7076fe3d4e43aed2f4e5f
miuckol <lanke.caroline45@gmail.com>
2024-09-26 18:22:04 -0300

    modify

--- trabalhofinalanalipe/database/migrations/2023_09_11_164314_create_postagem_table.php
+++ trabalhofinalanalipe/database/migrations/2023_09_11_164314_create_postagem_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string("titulo");
             $table->string("conteudo");
             $table->string("imagem")->nullable();
-            $table->int("curtidas");
+            $table->bigInteger("curtidas");
             $table->bigInteger("user_id")->unsigned();
             $table->timestamps();
           
