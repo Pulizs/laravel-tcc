bd035d6ad107fb0d231a895fb5980047f00a9c77
Pedro Cruz <pedrocs.flip@gmail.com>
2024-12-16 07:04:17 -0300

    update

--- trabalhofinalanalipe/resources/views/usuarios/edit.blade.php
+++ trabalhofinalanalipe/resources/views/usuarios/edit.blade.php
@@ -10,7 +10,6 @@
 		<br>
 
 		<center>
-			<div class="row">
 				<div class="col-md-4">
 					<div class="card">
 						<img src="images/test4.jpeg" class="card-img-top" alt="...">
@@ -45,50 +44,14 @@
 											<path
 												d="M13.879 10.414a2.501 2.501 0 0 0-3.465 3.465zm.707.707-3.465 3.465a2.501 2.501 0 0 0 3.465-3.465m-4.56-1.096a3.5 3.5 0 1 1 4.949 4.95 3.5 3.5 0 0 1-4.95-4.95ZM11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m.256 7a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z" />
 										</svg></button>
+										<button type="submit" class="btn btn-primary">Salvar</button>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div class="col">
-					<div class="row">
-						<div class="col">
-							<h6><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-									class="bi bi-calendar2" viewBox="0 0 16 16">
-									<path
-										d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1z" />
-									<path
-										d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5z" />
-								</svg> Entrou em {{ $usuario->created_at }}</h6>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col">
-							<h6><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-									class="bi bi-telephone" viewBox="0 0 16 16">
-									<path
-										d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
-								</svg> {{ $usuario->celular }}</h6>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col">
-							<h6><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-									class="bi bi-person-fill" viewBox="0 0 16 16">
-									<path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-								</svg> Nome Completo: {{ $usuario->nome }}</h6>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col">
-							<button type="button" class="btn btn-outline-secondary">Enviar mensagem</button>
-						</div>
-					</div>
-				</div>
-			</div>
 		</center>
 		<br><br>
-		<button type="submit" class="btn btn-primary">Salvar</button>
 
 	</form>
 </div>
