ee38a032e635301156b4301859687fbcd90cd437
miuckol <lanke.caroline45@gmail.com>
2024-12-15 22:27:02 -0300

    sobreNos

---
+++ trabalhofinalanalipe/resources/views/sobreNos/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app-master') 
+@section('content')
+
+<div clas="container">
+    <img src="images/envelope.png" width="8%" alt=""><br>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+
+    <center>
+
+    <h1 style="font-family: Quicksand;">Sobre Nos</h1>
+
+
+    <p class="text-center" style="font-family: Quicksand;">
+    Math-Science Space surgiu da necessidade de criação de um ambiente próprio de
+    aprendizado para a divulgação de eventos e acesso a conteúdos científicos. 
+    <br><br>
+    Visamos apresentar de maneira facilitada e atrativa os conceitos e materiais
+    referentes às áreas de ciências exatas, de forma a despertar o interesse pela
+    ciência, promovendo a educação e o pensamento crítico entre o público jovem
+    ao oferecer recursos educacionais que complementam o aprendizado formal,
+    fomentando assim uma relação mais próxima e positiva entre os jovens e as áreas
+    STEM (Ciência, Tecnologia, Engenharia e Matemática).
+    <br><br>
+    Math-Science Space é primordial para criar uma ponte entre os jovens e a ciência,
+    mostrando que o conhecimento é não apenas fundamental, mas também fascinante
+    e capaz de transformar vidas.
+    </p><br>
+</div>
+</div>
+</div>
+
+
+
+@endsection
